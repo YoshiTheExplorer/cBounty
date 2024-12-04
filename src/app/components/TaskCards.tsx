20bf43d9d275c91801273f42ad27b8ca7bfddae1
Colin Yamada <colin.yamada@gmail.com>
2024-12-04 11:49:15 -0600

    First Commit

---
+++ src/app/components/TaskCards.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { client } from "@/app/client";
+import Link from "next/link";
+import { getContract } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import { useReadContract } from "thirdweb/react";
+
+type CampaignCardProps = {
+    campaignAddress: string;
+    index: number;
+};
+
+export const TaskCards: React.FC<CampaignCardProps> = ({ campaignAddress, index }) => {
+    const contract = getContract({
+        client: client,
+        chain: baseSepolia,
+        address: campaignAddress,
+    });
+
+    // Get Task Name
+    const { data, isPending } = useReadContract({
+        contract,
+        method:
+          "function getList() view returns ((string name, string description, uint256 bounty, uint256 dueDate)[])",
+        params: [],
+    });
+
+    // Handle loading and error states
+    if (isPending) {
+        return <p>Loading tasks...</p>;
+    }
+
+    if (!data || data.length === 0) {
+        return <p>No tasks found for this campaign.</p>;
+    }
+
+    const taskName = data[index]?.name;
+    const dueDate = new Date(Number(data[index]?.dueDate) * 1000);
+    const taskDiscription = data[index]?.description;
+
+    return (
+            <div className="flex flex-col justify-between max-w-sm p-6 bg-black border border-slate-200 rounded-lg shadow">
+                <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight">{taskName}</h5>
+                    
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Due: {dueDate.toString()}</p>
+                </div>
+                
+                <Link
+                    href={`/${campaignAddress}`}
+                    passHref={true}
+                >
+                    <p className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        More Details
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </p>
+                </Link>
+            </div>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,128 @@
 "use client";
 
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
 
+import { ConnectButton, useActiveAccount, useReadContract} from "thirdweb/react";
+import { baseSepolia } from "thirdweb/chains";
+import { getContract } from "thirdweb";
+
+import { TaskCards } from "./components/TaskCards";
+import { TASKMANAGER_FACTORY } from "./constants/contracts";
+
 export default function Home() {
+  // Get User Address
+  const userAddress = useActiveAccount()?.address;
+  const userAddressString = userAddress || "0";
+
+  // Initialize the TASKMANAGER_FACTORY contract
+  const contract = getContract({
+    client: client,
+    chain: baseSepolia,
+    address: TASKMANAGER_FACTORY,
+  });
+
+  // Fetch the user's personal task manager address
+  const { data: contractAddress, isPending: isLoadingContract } = useReadContract({
+    contract,
+    method: "function getContract(address user) view returns (address)",
+    params: [userAddressString],
+  });
+
+  const taskManagerAddress = contractAddress || "0x3145B15F2842B5272381DA0B147a7417D1aE3Bc8"
+
+  //Login In Page
+  if(!userAddress){
+    return(<StartScreen />)
+  }
+
+  //Loading Screen
+  if(isLoadingContract){
+    return(<LoadingScreen />)
+  }
+
+  //Sign In Page
+  if(!contractAddress){
+    return(<CreateAccount 
+      userAddress={userAddress}
+    />)
+  }
+
+  //Main Page
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
+    <MainPage
+      contractAddress={taskManagerAddress}
+      userAddress={userAddress}
+    />
+  );
+}
 
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
+//new message here
+const here = 100;
+
+function MainPage({contractAddress, userAddress}: { contractAddress: string; userAddress: string;}) {
+  // Fetch the list of tasks from the user's task manager
+  const { data: tasks, isPending: isLoadingTasks } = useReadContract({
+    contract: getContract({
+      client: client,
+      chain: baseSepolia,
+      address: contractAddress,
+    }),
+    method: "function getList() view returns ((string name, string description, uint256 bounty, uint256 dueDate)[])",
+    params: [],
+  });
+
+  return (
+    <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
+      <div className="py-10">
+        <h1 className="text-4xl font-bold mb-4">Tasks:</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {!isLoadingTasks && tasks && tasks.length > 0 ? (
+            tasks.map((task, index) => (
+              <TaskCards
+                key={task.name}
+                campaignAddress={contractAddress}
+                index={index}
+              />
+            ))
+          ) : (
+            <p>No Campaigns {userAddress}</p>
+          )}
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
   );
 }
 
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
-    </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
+function CreateAccount({ userAddress }: { userAddress: string }) {
+  return(
+    <div className="py-20 flex flex-col items-center justify-center min-h-screen">
+      <p className="text-4xl font-bold mb-10 text-center">Please Create An Account</p>
+      <div>{userAddress}</div>
     </div>
   );
 }
 
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
+function LoadingScreen(){
   return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
+    <div className="py-20 flex flex-col items-center justify-center min-h-screen">
+      <p className="text-1xl font-bold mb-4 text-center">Loading Task Manager...</p>
+    </div>
+  );
+}
+
+function StartScreen(){
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      <div className="py-20 flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-10 text-center">Welcome to TaskChain</h1>
+        <h2 className="text-1xl font-bold mb-4 text-center">Please Connect Your Wallet</h2>
+        <div className="flex items-center justify-center w-full">
+          <ConnectButton
+            client={client}
+            appMetadata={{ name: "TaskChain" }}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
