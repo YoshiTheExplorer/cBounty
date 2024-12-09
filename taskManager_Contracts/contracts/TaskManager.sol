8ed2898ef1a7d02071a85e9c33a1ff1084251dac
Colin Yamada <colin.yamada@gmail.com>
2024-12-08 18:19:07 -0600

    Allowed Multiplayer Capability to Smart Contract

--- taskManager_Contracts/contracts/TaskManager.sol
+++ taskManager_Contracts/contracts/TaskManager.sol
@@ -2,62 +2,75 @@
 pragma solidity ^0.8.0;
 
 contract TaskManager {
-    address payable private owner;
+    address payable private admin;
 
     struct Task {
         string name;
         string description;
         uint256 bounty;
         uint256 dueDate;
+        address completedBy;
+        bool isCompleted;
     }
 
     Task[] private taskList;
 
-    constructor(address owner_address) {
-        owner = payable(owner_address);
+    constructor(address admin_address) {
+        admin = payable(admin_address);
     }
 
-    modifier onlyOwner(){
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
+    function addTask(string memory name, string memory description, uint256 dueDate) public payable {
+        require(msg.sender == admin, "Only admin can call this function");
+        taskList.push(Task(name, description, msg.value, dueDate, address(0), false));
     }
 
-    function addTask(string memory _name, string memory _description, uint256 _dueDate) public payable onlyOwner{
-        taskList.push(Task(_name, _description, msg.value, _dueDate));
+    function markTaskCompleted(uint256 _index) public {
+        require(_index < taskList.length, "Task does not exist");
+        Task storage task = taskList[_index];
+
+        require(!task.isCompleted, "Task already marked as completed");
+        task.completedBy = msg.sender;
+        task.isCompleted = true;
     }
 
-    function completeTask(uint _index) public onlyOwner{
-        require(_index < taskList.length, "Index out of bounds.");
+    function approveTaskCompletion(uint256 _index) public {
+        require(msg.sender == admin, "Only admin can call this function");
+        require(_index < taskList.length, "Task does not exist");
 
-        require(msg.sender == owner, "caller is not the owner");
-        payable(msg.sender).transfer(taskList[_index].bounty);
+        Task storage task = taskList[_index];
 
-        for(uint i = _index; i < taskList.length -1; i++){
+        if (!task.isCompleted) {
+            task.completedBy = admin;
+            task.isCompleted = true;
+        }
+
+        require(task.completedBy != address(0), "No valid completer for this task");
+        payable(task.completedBy).transfer(task.bounty);
+
+        for (uint256 i = _index; i < taskList.length - 1; i++) {
             taskList[i] = taskList[i + 1];
         }
         taskList.pop();
     }
 
-    function donateTask(uint _index, address charity) public onlyOwner{ //TODO Complete
-        require(_index < taskList.length, "Index out of bounds.");
-        payable(charity).transfer(taskList[_index].bounty);
+    function rejectTaskCompletion(uint256 _index) public {
+        require(msg.sender == admin, "Only admin can call this function");
+        require(_index < taskList.length, "Task does not exist");
 
-        for(uint i = _index; i < taskList.length -1; i++){
-            taskList[i] = taskList[i + 1];
-        }
-        taskList.pop();
+        Task storage task = taskList[_index];
+
+        require(task.isCompleted, "Task not marked as completed");
+
+        task.completedBy = address(0);
+        task.isCompleted = false;
     }
 
     function getList() public view returns(Task[] memory){
         return taskList;
     }
 
-    function getBalance() external view returns (uint) {
-        return address(this).balance;
-    }
-
-    function getOwner() public view returns(address){
-        return owner;
+    function getAdmin() public view returns(address){
+        return admin;
     }
 
 }
--- taskManager_Contracts/contracts/TaskManagerFactory.sol
+++ taskManager_Contracts/contracts/TaskManagerFactory.sol
@@ -3,25 +3,82 @@ pragma solidity ^0.8.0;
 import {TaskManager} from "./TaskManager.sol";
 
 contract TaskManagerFactory {
-    mapping(address/*user*/ => address/*contract*/) private taskManagers;
-
-    event TaskManagerCreated(address indexed user, address taskManagerAddress);
-
-    function createTaskManager() public {
-        require(!checkAccount(msg.sender), "TaskManager already exists for this address");
-        
-        TaskManager taskManager = new TaskManager(msg.sender);
-        taskManagers[msg.sender] = address(taskManager);
-
-        emit TaskManagerCreated(msg.sender, address(taskManager));
+    struct TaskManagerInfo {
+        string name;
+        address owner;
+        bytes32 password;
+        address taskManagerAddress;
+        mapping(address => bool) members;
     }
 
-    function checkAccount(address user) public view returns (bool) {
-        return taskManagers[user] != address(0);
+    mapping(address => address[]) private userTaskManagers; //Users List of TaskManagers
+    mapping(address => TaskManagerInfo) private taskManagers; //List of All TaskManagers
+
+    function createTaskManager(string memory name, string memory password) public {
+        TaskManager taskManager = new TaskManager(msg.sender); //Link to Owner
+
+        TaskManagerInfo storage taskManagerInfo = taskManagers[address(taskManager)];
+
+        taskManagerInfo.name = name;
+        taskManagerInfo.owner = msg.sender;
+        taskManagerInfo.password = keccak256(abi.encodePacked(password));
+        taskManagerInfo.taskManagerAddress = address(taskManager);
+        taskManagerInfo.members[msg.sender] = true;
+
+        userTaskManagers[msg.sender].push(address(taskManager));
     }
 
-    function getContract(address user) public view returns (address) {
-        return taskManagers[user];
+    function updatePassword(address taskManagerAddress, string memory newPassword) public {
+        TaskManagerInfo storage taskManagerInfo = taskManagers[taskManagerAddress];
+
+        require(taskManagerInfo.taskManagerAddress != address(0), "TaskManager does not exist");
+        require(msg.sender == taskManagerInfo.owner, "Only the owner can update the password");
+
+        taskManagerInfo.password = keccak256(abi.encodePacked(newPassword));
+    }
+
+    function joinTaskManager(address taskManagerAddress, string memory password) public {
+        TaskManagerInfo storage taskManagerInfo = taskManagers[taskManagerAddress];
+
+        require(taskManagerInfo.taskManagerAddress != address(0), "TaskManager does not exist");
+        require(
+            taskManagerInfo.password == keccak256(abi.encodePacked(password)),
+            "Incorrect password"
+        );
+
+        taskManagerInfo.members[msg.sender] = true;
+        userTaskManagers[msg.sender].push(taskManagerAddress);
+    }
+
+    function leaveTaskManager(address taskManagerAddress) public {
+        TaskManagerInfo storage taskManagerInfo = taskManagers[taskManagerAddress];
+
+        require(taskManagerInfo.members[msg.sender], "You are not a member of this TaskManager");
+
+        taskManagerInfo.members[msg.sender] = false;
+        address[] storage userTaskManagerList = userTaskManagers[msg.sender];
+        for (uint256 i = 0; i < userTaskManagerList.length; i++) {
+            if (userTaskManagerList[i] == taskManagerAddress) {
+                userTaskManagerList[i] = userTaskManagerList[userTaskManagerList.length - 1];
+                userTaskManagerList.pop();
+                break;
+            }
+        }
+    }
+
+    function getUserTaskManagers(address user) public view returns (address[] memory) {
+        return userTaskManagers[user];
+    }
+
+    function getTaskManagerDetails(address taskManagerAddress) public view returns (string memory name, address owner, bool isMember) {
+        TaskManagerInfo storage taskManagerInfo = taskManagers[taskManagerAddress];
+        require(taskManagerInfo.taskManagerAddress != address(0), "TaskManager does not exist");
+
+        return (
+            taskManagerInfo.name,
+            taskManagerInfo.owner,
+            taskManagerInfo.members[msg.sender]
+        );
     }
 
 }
